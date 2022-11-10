14e825f928aca791c1f652a2c233395ab3183b1a
Yahoo <yac033@ucsd.edu>
2022-11-10 12:57:18 -0800

    Fixed the bug that unable to retreat the recipes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,12 +68,15 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
-  const existRecipes = localStorage.getItem('recipes');
+  const existRecipes = JSON.parse(localStorage.getItem('recipes'));
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
   let requestRecipes = [];
+  if(existRecipes != null){
+    requestRecipes.push(existRecipes);
+  }
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
