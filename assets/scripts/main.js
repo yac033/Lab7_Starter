d1e6ae97a7a4816c92472b5b7a83681e9c87f824
Yahoo <yac033@ucsd.edu>
2022-11-13 13:40:20 -0800

    Fix the reg SW

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,9 +52,7 @@ function initializeServiceWorker() {
   // B2. TODO - Listen for the 'load' event on the window object.
   window.addEventListener('load',async ()=>{
     try{
-      const registration = await navigator.serviceWorker.register("./sw.js",{
-        scope: "/",
-      });
+      const registration = await navigator.serviceWorker.register("sw.js");
       if(registration.active){
         console.log("service worker has been successfully registered");
       }
